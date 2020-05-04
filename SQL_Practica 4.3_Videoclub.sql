88912baedd0f9eff6cc5e01318083b8fb2770461
q92helea <q92helea@hotmail.com>
2020-05-04 10:00:17 +0200

    Inicio

---
+++ SQL_Practica 4.3_Videoclub.sql
@@ -0,0 +1,43 @@
+
+
+CREATE TABLE director (
+Nombre VARCHAR2(30) PRIMARY KEY,
+Nacionalidad VARCHAR(15)
+);
+CREATE TABLE Actores (
+Nombre VARCHAR2(30) PRIMARY KEY,
+Nacionalidad VARCHAR2(15),
+Sexo VARCHAR2(1)
+);
+CREATE TABLE Película (
+ID_Peli VARCHAR2(5)PRIMARY KEY,
+Título VARCHAR2(30),
+Productora VARCHAR2(30),
+Nacionalidad VARCHAR2(15),
+Fecha DATE,
+Nombre VARCHAR2(30),
+CONSTRAINT fk_Nombre FOREIGN KEY (Nombre) REFERENCES director(Nombre)
+);
+
+CREATE TABLE actua (
+Actor VARCHAR2(30),
+ID_film VARCHAR2(4),
+CONSTRAINT FK_Actor FOREIGN KEY (Actor) REFERENCES Actores (Nombre),
+CONSTRAINT FK_ID_film FOREIGN KEY (ID_film) REFERENCES Película(ID_Peli)
+);
+
+CREATE TABLE socio (
+DNI VARCHAR2(9),
+Nombre VARCHAR2(15)NOT NULL
+Dirección VARCHAR2(30),
+Teléfono VARCHAR2(9),
+Avalador VARCHAR2(15),
+CONSTRAINT FK_avalador_socio FOREIGN KEY (Avalador) REFERENCES socio(DNI)
+);
+
+CREATE TABLE alquila (
+DNI VARCHAR2(9),
+ID_Peli VARCHAR2(5),
+N
+
+
